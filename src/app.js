aa01d3c98cfcf37287cac061d86c4f150ca1ab3c
Bryan <bguerre12@gmail.com>
2015-05-21 00:42:39 -0700

    Functional Time Requests
    Added getting time until next bus stop at some random bus stop? route 63 1358 stop.. not sure which one that is, need to change to 171 la pasada, and 171 main campus drive

--- src/app.js
+++ src/app.js
@@ -1,11 +1,5 @@
-/**
- * Welcome to Pebble.js!
- *
- * This is where you write your app.
- */
-
 var UI = require('ui');
-var Vector2 = require('vector2');
+var gCountdown = {};
 
 var main = new UI.Card({
   title: '   Bus Watcher',
@@ -14,6 +8,7 @@ var main = new UI.Card({
   body: ' - Top for Stop 1\n - Bottom for\n   Stop 2'
 });
 
+fetchBusTime1();
 main.show();
 
 main.on('click', 'up', function(e) {
@@ -22,10 +17,14 @@ main.on('click', 'up', function(e) {
   card.subtitle('Apt. Stop');
   card.body('\nFetching...\nPlease Wait.');
   card.show();
+  
+  fetchBusTime1();
+  
+  card.body(gCountdown);
 });
 
 main.on('click', 'select', function(e) {
-  
+  //do nothing
 });
 
 main.on('click', 'down', function(e) {
@@ -34,4 +33,53 @@ main.on('click', 'down', function(e) {
   card.subtitle('Campus Stop');
   card.body('\nFetching...\nPlease Wait.');
   card.show();
+  
+  fetchBusTime1();
+  
+  card.body(gCountdown);
 });
+
+
+ function fetchBusTime1(){
+   
+   var isInitMsg = true;
+   var response;
+   var req = new XMLHttpRequest();
+  // build the GET request
+    req.open('POST', "http://webwatch.lbtransit.com/tmwebwatch/Arrivals.aspx/getStopTimes", true);
+    req.setRequestHeader("Content-Type","application/json");
+  req.onload = function(e) {
+    if (req.readyState == 4) {
+      // 200 - HTTP OK
+      if(req.status == 200) {
+        console.log(req.responseText);
+        response = JSON.parse(req.responseText);
+        var predTime, predPeriod, countdown;
+        if (response.d) {
+          // data found, look for LastPrice
+            predTime = response.d.stops[0].crossings[0].predTime;
+            predPeriod = response.d.stops[0].crossings[0].predPeriod;
+            countdown = response.d.stops[0].crossings[0].countdown;
+          console.log(predTime);
+          gCountdown.init = true;
+          gCountdown.symbol = "121";
+          var msg = {};
+          if (isInitMsg) {
+            msg.init = true;
+            //msg.symbol = symbol;
+            msg.symbol = "121";
+          }
+          //msg.price = predTime.toString()+" "+predPeriod.toString();
+            msg.price = countdown.toString();
+          gCountdown = countdown.toString();
+          Pebble.sendAppMessage(msg);
+        }
+      } else {
+        console.log("Request returned error code " + req.status.toString());
+      }
+    }
+  };
+  req.send("{routeID: 63,       directionID: 4, stopID: 1348, useArrivalTimes:  true}");
+    //CHANGE THIS LINE TO GET OTHER STOPS
+ }
+
